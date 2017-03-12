1572721f1eb9497d367488f0c82f7f67b7d5aabb
Anbo Wei <anbo_wei@outlook.com>
2017-03-11 22:48:01 -0700

    i suck

--- static/js/phoneScene.js
+++ static/js/phoneScene.js
@@ -166,7 +166,7 @@ function handleOrientation(event) {
 		player.camera.setRoll(beta);
 		player.camera.setPitch(gamma);
 
-		output.innerHTML = "alpha: " + alpha + "\nbeta: " + beta + "\ngamma: " + gamma;
+		document.querySelector('.output').innerHTML = "alpha: " + alpha + "\nbeta: " + beta + "\ngamma: " + gamma;
 	}
 }
 
