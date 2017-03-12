5a70cc743ab084abd9b23cf7ef8af1efe00a9e54
smchiang <hyperchocolategirl@gmail.com>
2017-03-12 12:56:17 -0700

    switch to roll

--- static/js/phoneScene.js
+++ static/js/phoneScene.js
@@ -163,8 +163,8 @@ function handleOrientation(event) {
 	var gamma = event.gamma; // Corresponding to pitch
 	console.log("alpha: " + alpha + "\nbeta: " + beta + "\ngamma: " + gamma);
 
-	player.camera.setYaw(alpha);
-	//player.camera.setRoll(beta);
+	//player.camera.setYaw(alpha);
+	player.camera.setRoll(beta);
 	//player.camera.setPitch(gamma);
 
 	$("#output").html("event.absolute: " + event.absolute + "\nalpha: " + alpha + "\nbeta: " + beta + "\ngamma: " + gamma);
