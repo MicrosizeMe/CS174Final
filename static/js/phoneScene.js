8e742d359b007129a3066c7cef4c6d02e20c7884
Anbo Wei <anbo_wei@outlook.com>
2017-03-11 23:00:54 -0700

    help me

--- static/js/phoneScene.js
+++ static/js/phoneScene.js
@@ -156,19 +156,19 @@ var glHelper = (function() {
 })();
 
 function handleOrientation(event) {
-	alert("Lol");
-	if (event.absolute) {
+	// alert("Lol");
+	// if (event.absolute) {
 		var alpha = event.alpha; // Corresponding to yaw
 		var beta = (90 - abs(event.beta)) * ((event.beta > 0) ? 1 : -1); // Corresponding to roll
 		var gamma = event.gamma; // Corresponding to pitch
-		alert("alpha: " + alpha + "\nbeta: " + beta + "\ngamma: " + gamma);
+		// alert("alpha: " + alpha + "\nbeta: " + beta + "\ngamma: " + gamma);
 
 		player.camera.setYaw(alpha);
 		player.camera.setRoll(beta);
 		player.camera.setPitch(gamma);
 
-		// $("#output").text("alpha: " + alpha + "\nbeta: " + beta + "\ngamma: " + gamma);
-	}
+		$("#output").text("alpha: " + alpha + "\nbeta: " + beta + "\ngamma: " + gamma);
+	// }
 }
 
 // Init function to start GL and draw everything
