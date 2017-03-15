0b616e08bde552fe83459dc25a2146ae4a1d7a8a
Vamsi Mokkapati <mvd707@gmail.com>
2017-03-14 18:13:03 -0700

    smaller floating cubes

--- static/js/objects/tree.js
+++ static/js/objects/tree.js
@@ -90,8 +90,8 @@ Tree.prototype.draw = function(dt, mat) {
     this.foliageRound.position  = add(pos, vec3(0.0, 1.0 * kY, 0.0));
     this.foliageRound.scale		= vec3(8.7 * kX, 8.5 * kY, 8.7 * kZ);
     
-    this.memeSquare.position  = add(pos, vec3(1.0 * kX, 0.0, 0.0));
-    this.memeSquare.scale		= vec3(1 * kX, 1 * kY, 1 * kZ);
+    this.memeSquare.position  = add(pos, vec3(1.0 * kX, 3.0, 0.0));
+    this.memeSquare.scale		= vec3(0.25 * kX, 0.25 * kY, 0.25 * kZ);
     
     this.trunk.draw(dt, mat);
     this.foliageRound.draw(dt, mat);
